502ea8001228b4db81571124277be9b925a24e62
ismayil0990 <111593973+ismayil0999@users.noreply.github.com>
2023-02-18 00:04:45 +0400

    Add files via upload

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Routes, Route } from 'react-router-dom';
+import { Container } from '../styled/styled';
+import Chat from './Chat';
+import LeftSidebar from './LeftSidebar';
+import Menu from './Menu';
+import RightSidebar from './RightSidebar';
+export default function Body() {
+  return (
+   <Container>
+       <LeftSidebar/>
+    <RightSidebar/>
+   </Container>
+  );
+}
